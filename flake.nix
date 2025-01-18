963f1008ba20aba025f529651c9604ef71207526
Source-Kod <Source-Kod@protonmail.com>
2025-01-17 21:44:27 -0600

    setup

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "For odin memory cards";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default =
+        pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              nodejs
+              typescript
+            ];
+          };
+    };
+}
